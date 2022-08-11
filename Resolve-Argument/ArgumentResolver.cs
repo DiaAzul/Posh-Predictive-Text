416d3e13508704245162eb40fb9ddf1b6b9d10aa
David Plummer <28156158+DiaAzul@users.noreply.github.com>
2022-08-11 11:22:58 +0100

    Refactored loading of syntax tree to separate class.
    Implemented cache of syntax trees using a dictionary within the Argument Resolver.

--- Resolve-Argument/ArgumentResolver.cs
+++ Resolve-Argument/ArgumentResolver.cs
@@ -2,12 +2,7 @@
 namespace ResolveArgument
 {
     using System.Linq;
-    using System.Xml.Linq;
     using System.Management.Automation;
-    using System.Management.Automation.Language;
-    using System.Reflection;
-    using System.IO;
-    using System;
 
     /// <summary>
     /// Record within the command syntax tree.
@@ -37,99 +32,35 @@ namespace ResolveArgument
     /// </summary>
     internal class ArgumentResolver
     {
-        private static XDocument? syntaxTree;
+        /// <summary>
+        /// Each command has a syntax tree which sets out the possible combination of tokens
+        /// on the command line. The trees are strored as XML resource files embeded within
+        /// the application. These are loaded, parsed and converted to a list of Syntax items
+        /// when a command requires tab-completions.
+        /// </summary>
+        private static readonly Dictionary<string, List<SyntaxItem>> syntaxTrees = new();
 
 
         /// <summary>
-        /// Load the syntax tree for which completions required.
+        /// Loads the syntax tree for a named command into the dictionary of syntax trees.
+        /// 
+        /// The method reads the XML file embeded within the application, parses it
         /// </summary>
         /// <param name="syntaxTreeName">Name of syntax tree to load.</param>
         internal static void LoadSyntaxTree(string syntaxTreeName)
-        {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            try
+        { 
+            // Load syntax tree
+            var syntaxTree = SyntaxTree.Load(syntaxTreeName);
+
+            // If the syntax tree loaded than add to the dictionary.
+            if (syntaxTree.Any())
             {
-                var resourceStream = assembly.GetManifestResourceStream("Resolve_Argument.SyntaxTrees.CondaSyntaxTree.xml");
-
-                if (resourceStream != null)
-                {
-                    using StreamReader reader = new(resourceStream);
-                    var xmlDoc = reader.ReadToEnd();
-                    syntaxTree = XDocument.Parse(xmlDoc);
-                }
+                syntaxTrees[syntaxTreeName] = syntaxTree;
             }
-            catch (FileNotFoundException)
+            else
             {
-                LOGGER.Write("File not found.");
+                syntaxTrees.Remove(syntaxTreeName);
             }
-            catch (BadImageFormatException)
-            {
-                LOGGER.Write("File wrong format.");
-            }
-            catch (FileLoadException)
-            {
-                LOGGER.Write("File was found, could not load.");
-            }
-
-        }
-
-        /// <summary>
-        /// Convert nullable <c>XElement</c> node in an XML tree to a <c>string</c>.
-        /// 
-        /// Null values are converted to an empty string.
-        /// </summary>
-        /// <param name="element">Node in XML tree.</param>
-        /// <returns>Contents of node as string.</returns>
-        internal static string AsString(XElement? element)
-        {
-            string elementAsString = "";
-            if (element != null)
-            {
-                elementAsString = (string)element;
-            }
-
-            return elementAsString;
-        }
-
-        /// <summary>
-        /// Convert nullable <c>XElement</c> node in an XML tree to a nullable <c>string</c>.
-        /// 
-        /// Null values are converted to an empty string.
-        /// </summary>
-        /// <param name="element">Node in XML tree.</param>
-        /// <returns>Contents of node as string.</returns>
-        internal static string? AsNullableString(XElement? element)
-        {
-            string? elementAsString = null;
-            if (element != null)
-            {
-                elementAsString = (string)element;
-            }
-
-            return elementAsString;
-        }
-
-        /// <summary>
-        /// Convert nullable <c>XElement</c> node in an XML tree to a nullable <c>bool</c>.
-        /// 
-        /// <para>The method returns true if the content of the node matches the test pattern. The
-        /// default matching pattern is <c>TRUE</c>.</para>
-        /// 
-        /// <para>If the node is null then the method returns a null bool.</para>
-        /// </summary>
-        /// <param name="element">Node in XML tree.</param>
-        /// <param name="trueValue">Test pattern for true value. Default <c>TRUE</c>.</param>
-        /// <returns>True when the contents of the node match the test pattern. Null if the node is null.</returns>
-        internal static bool? AsNullableBool(XElement? element, string trueValue = "TRUE")
-        {
-            bool? elementAsNullableBool = null;
-            if (element != null)
-            {
-                elementAsNullableBool = (string)element == trueValue;
-            }
-
-            return elementAsNullableBool;
-
         }
 
         /// <summary>
@@ -139,9 +70,22 @@ namespace ResolveArgument
         /// <returns>True if syntax tree is loaded.</returns>  
         internal static bool SyntaxTreeExists(string syntaxTreeName)
         {
-            return syntaxTree != null;
+            return syntaxTrees.ContainsKey(syntaxTreeName);
         }
-        
+
+        /// <summary>
+        /// Gets the syntax tree name from the base command token.
+        /// 
+        /// This method should text that the command is valid and resolve
+        /// any aliases to the correct tree.
+        /// </summary>
+        /// <param name="baseCommandToken"></param>
+        /// <returns>Syntax tree name.</returns>
+        internal static string SyntaxTreeName(Token baseCommandToken)
+        {
+            // TODO: Manage aliases for the syntax tree (e.g. mamba -> conda).
+            return baseCommandToken.text;
+        }
         /// <summary>
         /// Processes the command line tokens and suggests completions for the wordToComplete.
         /// </summary>
@@ -162,92 +106,45 @@ namespace ResolveArgument
 
             if (commandTokens.BaseCommand != null)
             {
-                var baseCommand = (Token) commandTokens.BaseCommand;
-                if (!SyntaxTreeExists(baseCommand.text)) LoadSyntaxTree(baseCommand.text);
+                var baseCommand = (Token)commandTokens.BaseCommand;
+                string syntaxTreeName = baseCommand.text;
 
+                // If the syntax tree does not exist then try and load it.
+                if (!SyntaxTreeExists(syntaxTreeName)) LoadSyntaxTree(syntaxTreeName);
 
-                // TODO Resolve where we are in the token tree. First token (Link search for exact matches). -> Next options
-                // Search next options -> if no returns then previous search has the options available.
-
-                LOGGER.Write("Creating query.");
-
-                LOGGER.Write($"The syntaxTree exists: {syntaxTree != null}");
-
-                XElement? root = syntaxTree?.Root;
-
-                LOGGER.Write($"The root exists: {root != null}");
-
-                IEnumerable<SyntaxItem>? commandQuery = null;
-                if (root != null)
+                // If successfully loaded then continue to process suggestions.
+                if (SyntaxTreeExists(syntaxTreeName))
                 {
-                    try
-                    {
-                        commandQuery = from item in root.Elements("item")
-                                       select new SyntaxItem
-                                       {
-                                           command = AsString(item.Element("CMD")),
-                                           commandPath = AsString(item.Element("PATH")),
-                                           type = AsString(item.Element("TYPE")),
-                                           argument = AsNullableString(item.Element("ARG")),
-                                           alias = AsNullableString(item.Element("AL")),
-                                           multipleUse = AsNullableBool(item.Element("MU")),
-                                           parameter = AsNullableString(item.Element("PRM")),
-                                           multipleParameters = AsNullableBool(item.Element("MP")),
-                                           toolTip = AsNullableString(item.Element("TT"))
-                                       };
-                        LOGGER.Write($"Hi! The command query exists: {commandQuery != null}");
-                    }
-                    catch (Exception e)
-                    {
-                        LOGGER.Write(e.ToString());
-                    }
+                    List<SyntaxItem> syntaxTree = syntaxTrees[syntaxTreeName];
+                    LOGGER.Write($"The syntaxTree exists. Length: {syntaxTree.Count}");
+
+
+                    // total_tokens = commandTokens.Length
+                    // commandTokensIndex = 0
+                    // Command_Path = ""
+                    // Get token[commandTokensIndex]
+                    // If Type = System.Management.Automation.ParameterAst -> 
+                    // Either another command or positional parameter.
+
+
+                    // TODO Remove dummy test code.
+                    CompletionResult response = new(
+                        WordToComplete + "a",
+                        WordToComplete + "a",
+                        CompletionResultType.ParameterName, //Need to change this for ast.
+                        "ToolTip");
+
+                    CompletionResult response2 = new(
+                        WordToComplete + "b",
+                        WordToComplete + "b",
+                        CompletionResultType.ParameterName, //Need to change this for ast.
+                        "ToolTip");
+
+                    suggestions.Add(response);
+                    suggestions.Add(response2);
+
+                    LOGGER.Write($"Providing suggestions...{WordToComplete}a, b.");
                 }
-                LOGGER.Write($"Command query exists: {commandQuery != null}");
-                LOGGER.Write("Executing query.");
-                List<SyntaxItem> list = new();
-
-                if (commandQuery != null)
-                {
-                    try
-                    {
-                        list = commandQuery.ToList();
-                    }
-                    catch (Exception e)
-                    {
-                        LOGGER.Write(e.ToString());
-                    }
-                }
-
-                LOGGER.Write($"List length = {list.Count}");
-
-                var firstItem = list[0];
-
-                LOGGER.Write($"First list item: {firstItem.AsString()}");
-
-                // total_tokens = commandTokens.Length
-                // commandTokensIndex = 0
-                // Command_Path = ""
-                // Get token[commandTokensIndex]
-                // If Type = System.Management.Automation.ParameterAst -> 
-                // Either another command or positional parameter.
-
-
-                CompletionResult response = new(
-                    WordToComplete + "a",
-                    WordToComplete + "a",
-                    CompletionResultType.ParameterName, //Need to change this for ast.
-                    "ToolTip");
-
-                CompletionResult response2 = new(
-                    WordToComplete + "b",
-                    WordToComplete + "b",
-                    CompletionResultType.ParameterName, //Need to change this for ast.
-                    "ToolTip");
-
-                suggestions.Add(response);
-                suggestions.Add(response2);
-
-                LOGGER.Write($"Providing suggestions...{WordToComplete}a, b.");
             }
 
             return suggestions;
---
+++ Resolve-Argument/LoadSyntaxTree.cs
@@ -0,0 +1,149 @@
+﻿
+namespace ResolveArgument
+{
+    using System.Reflection;
+    using System.Xml.Linq;
+    internal class SyntaxTree
+    {
+        /// <summary>
+        /// Loads the syntax tree for a named command into the dictionary of syntax trees.
+        /// 
+        /// The method reads the XML file embeded within the application, parses it
+        /// </summary>
+        /// <param name="syntaxTreeName">Name of syntax tree to load.</param>
+        internal static List<SyntaxItem> Load(string syntaxTreeName)
+        {
+            // Load XML File from assembly into XDocument.
+            XDocument? xSyntaxTree = null;
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            try
+            {
+                // TODO: This needs to vary according to the syntax tree to be loaded.
+                var resourceStream = assembly.GetManifestResourceStream("Resolve_Argument.SyntaxTrees.CondaSyntaxTree.xml");
+
+                if (resourceStream != null)
+                {
+                    using StreamReader reader = new(resourceStream);
+                    var xmlDoc = reader.ReadToEnd();
+                    xSyntaxTree = XDocument.Parse(xmlDoc);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                LOGGER.Write("File not found.");
+            }
+            catch (BadImageFormatException)
+            {
+                LOGGER.Write("File wrong format.");
+            }
+            catch (FileLoadException)
+            {
+                LOGGER.Write("File was found, could not load.");
+            }
+
+            // Parse the XML document into a List.
+            List<SyntaxItem> syntaxTree = new();
+            IEnumerable<SyntaxItem>? syntaxTreeQuery = null;
+            XElement? root = xSyntaxTree?.Root;
+
+            if (root != null)
+            {
+                try
+                {
+                    syntaxTreeQuery = from item in root.Elements("item")
+                                      select new SyntaxItem
+                                      {
+                                          command = AsString(item.Element("CMD")),
+                                          commandPath = AsString(item.Element("PATH")),
+                                          type = AsString(item.Element("TYPE")),
+                                          argument = AsNullableString(item.Element("ARG")),
+                                          alias = AsNullableString(item.Element("AL")),
+                                          multipleUse = AsNullableBool(item.Element("MU")),
+                                          parameter = AsNullableString(item.Element("PRM")),
+                                          multipleParameters = AsNullableBool(item.Element("MP")),
+                                          toolTip = AsNullableString(item.Element("TT"))
+                                      };
+                }
+                catch (Exception e)
+                {
+                    LOGGER.Write(e.ToString());
+                }
+            }
+
+            if (syntaxTreeQuery != null)
+            {
+                try
+                {
+                    syntaxTree = syntaxTreeQuery.ToList();
+                }
+                catch (Exception e)
+                {
+                    LOGGER.Write(e.ToString());
+                }
+            }
+
+            return syntaxTree;
+        }
+
+        /// <summary>
+        /// Convert nullable <c>XElement</c> node in an XML tree to a <c>string</c>.
+        /// 
+        /// Null values are converted to an empty string.
+        /// </summary>
+        /// <param name="element">Node in XML tree.</param>
+        /// <returns>Contents of node as string.</returns>
+        internal static string AsString(XElement? element)
+        {
+            string elementAsString = "";
+            if (element != null)
+            {
+                elementAsString = (string)element;
+            }
+
+            return elementAsString;
+        }
+
+        /// <summary>
+        /// Convert nullable <c>XElement</c> node in an XML tree to a nullable <c>string</c>.
+        /// 
+        /// Null values are converted to an empty string.
+        /// </summary>
+        /// <param name="element">Node in XML tree.</param>
+        /// <returns>Contents of node as string.</returns>
+        internal static string? AsNullableString(XElement? element)
+        {
+            string? elementAsString = null;
+            if (element != null)
+            {
+                elementAsString = (string)element;
+            }
+
+            return elementAsString;
+        }
+
+        /// <summary>
+        /// Convert nullable <c>XElement</c> node in an XML tree to a nullable <c>bool</c>.
+        /// 
+        /// <para>The method returns true if the content of the node matches the test pattern. The
+        /// default matching pattern is <c>TRUE</c>.</para>
+        /// 
+        /// <para>If the node is null then the method returns a null bool.</para>
+        /// </summary>
+        /// <param name="element">Node in XML tree.</param>
+        /// <param name="trueValue">Test pattern for true value. Default <c>TRUE</c>.</param>
+        /// <returns>True when the contents of the node match the test pattern. Null if the node is null.</returns>
+        internal static bool? AsNullableBool(XElement? element, string trueValue = "TRUE")
+        {
+            bool? elementAsNullableBool = null;
+            if (element != null)
+            {
+                elementAsNullableBool = (string)element == trueValue;
+            }
+
+            return elementAsNullableBool;
+
+        }
+
+    }
+}
