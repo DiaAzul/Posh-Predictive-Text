52758f915033ac9e7f5ca8badcf02ddbfb170316
David Plummer <28156158+DiaAzul@users.noreply.github.com>
2022-08-12 15:26:32 +0100

    Revert log level to INFO for testing.

--- Debug-LocalImport-Module.ps1
+++ Debug-LocalImport-Module.ps1
@@ -1,5 +1,5 @@
 ﻿# Script to import and install the debug version of the module.
 
 Import-Module ".\Resolve-Argument\bin\Debug\net6.0\Resolve-Argument.dll"
-Resolve-Argument -Initialise -LogFile "C:\templogfiles\logfile.txt" -LogLevel "ERROR" | Invoke-Expression
+Resolve-Argument -Initialise -LogFile "C:\templogfiles\logfile.txt" -LogLevel "INFO" | Invoke-Expression
 
