f9e3856f7fe79d1aeeea990d3e3009fd3b9a1b11
David Plummer <28156158+DiaAzul@users.noreply.github.com>
2022-08-20 22:37:12 +0100

    Add documentation to CondaHelpers class and GetEnvironments method.

--- Resolve-Argument/Helpers/CondaHelpers.cs
+++ Resolve-Argument/Helpers/CondaHelpers.cs
@@ -3,8 +3,16 @@
 namespace Resolve_Argument.Helpers
 {
     using ResolveArgument;
+
+    /// <summary>
+    /// Helper classes to provide additional completion suggestion for conda commands.
+    /// </summary>
     internal static class CondaHelpers
     {
+        /// <summary>
+        /// Get a list of conda environments available on this computer.
+        /// </summary>
+        /// <returns>List of conda environments.</returns>
         internal static List<string> GetEnvironments()
         {
             List<string> environments = new();
