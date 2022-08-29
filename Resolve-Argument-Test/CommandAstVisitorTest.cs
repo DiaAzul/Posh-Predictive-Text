4a24a3eb5323d30144dbeeb0aa2feaa3542e4104
David Plummer <28156158+DiaAzul@users.noreply.github.com>
2022-08-29 22:16:15 +0100

    Update TODOs : Build lists of functions that will require tests.

---
+++ Resolve-Argument-Test/CommandAstVisitorTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation.Language;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resolve_Argument_Test
+{
+    // TODO [ ][TEST] Token
+    // TODO [ ][TEST] CommandAstVisitor
+ 
+    internal class CommandAstVisitorTest
+    {
+    }
+}
+
+
+// internal record Token
+//internal string Value { get; init; } = default!;
+//internal Type Type { get; init; } = typeof(StringConstantExpressionAst);
+//internal bool IsCommand
+//internal bool IsCommandExpression
+//internal bool IsCommandParameter
+//internal bool IsStringConstantExpression
+
+
+
+// internal class CommandAstVisitor : AstVisitor
+//internal CommandAstVisitor()
+//private int TokenPosition
+//internal string? BaseCommand
+//internal Token? LastToken
+//internal Token? PriorToken
+//internal Dictionary<int, Token> All
+//internal int Count
+//internal Dictionary<int, Token> CommandParameters
+//internal Token? Index(int index)
+//internal (string, int) CommandPath(List<string> uniqueCommands)
+//internal bool CanUse(SyntaxItem syntaxItem)
+//public override AstVisitAction DefaultVisit(Ast ast)
+//public override AstVisitAction VisitCommand(CommandAst commandAst)
+//public override AstVisitAction VisitCommandExpression(CommandExpressionAst commandExpressionAst)
+//public override AstVisitAction VisitCommandParameter(CommandParameterAst commandParameterAst)
+//public override AstVisitAction VisitStringConstantExpression(StringConstantExpressionAst stringConstantExpressionAst)
+
---
+++ Resolve-Argument-Test/SyntaxTreeConfigTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resolve_Argument_Test
+{
+    //TODO [ ][TEST] Syntax Tree Config
+    internal class SyntaxTreeConfigTest
+    {
+        // Supported Commands
+        // Command From Alias
+        // Definition
+        // ToolTips
+    }
+}
