8984c773dd799454f55fb3b489e62239d2680a16
David Plummer <28156158+DiaAzul@users.noreply.github.com>
2022-09-02 17:39:26 +0100

    New Feature: PSReadLine predictor.

---
+++ PoshPredictiveText/PoshPredictiveTextPredictor.cs
@@ -0,0 +1,170 @@
+﻿
+
+namespace PoshPredictiveText
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Management.Automation;
+    using System.Management.Automation.Subsystem;
+    using System.Management.Automation.Subsystem.Prediction;
+
+    /// <summary>
+    /// PSReadLine plugin providing predictive text capabilities.
+    /// </summary>
+    public class PoshPredictiveTextPredictor : ICommandPredictor
+    {
+        private readonly Guid _guid;
+
+        /// <summary>
+        /// Initialise a new guid.
+        /// </summary>
+        /// <param name="guid">Guid passed from PSReadLine</param>
+        internal PoshPredictiveTextPredictor(string guid)
+        {
+            _guid = new Guid(guid);
+        }
+
+        /// <summary>
+        /// Gets the unique identifier for a subsystem implementation.
+        /// </summary>
+        public Guid Id => _guid;
+
+        /// <summary>
+        /// Gets the name of a subsystem implementation.
+        /// </summary>
+        public string Name => "PoshPredictiveText";
+
+        /// <summary>
+        /// Gets the description of a subsystem implementation.
+        /// </summary>
+        public string Description => "Predictive text plug-in for PSReadLine";
+
+        /// <summary>
+        /// Get the predictive suggestions. It indicates the start of a suggestion rendering session.
+        /// </summary>
+        /// <param name="client">Represents the client that initiates the call.</param>
+        /// <param name="context">The <see cref="PredictionContext"/> object to be used for prediction.</param>
+        /// <param name="cancellationToken">The cancellation token to cancel the prediction.</param>
+        /// <returns>An instance of <see cref="SuggestionPackage"/>.</returns>
+        public SuggestionPackage GetSuggestion(
+            PredictionClient client,
+            PredictionContext context,
+            CancellationToken cancellationToken)
+        {
+            // If nothing on the command line return.            
+            string inputText = context.InputAst.Extent.Text;
+            var cursorPosition = context.CursorPosition.Offset;
+
+            if (string.IsNullOrWhiteSpace(inputText)) return default;
+
+            // If there is no abstract syntax tree return.
+            var promptAst = context.RelatedAsts[0];
+            if (promptAst is null) return default;
+
+            // Tokenise the syntax tree.
+            var enteredTokens = new CommandAstVisitor();
+            promptAst.Visit(enteredTokens);
+
+            // If there is no base command, or the base command is not supported then return.
+            if (enteredTokens.BaseCommand is null) return default;
+            if (!SyntaxTreesConfig.IsSupportedCommand(enteredTokens.BaseCommand)) return default;
+
+            // If last character is a space then add an extra empty token to trigger suggestions of next token.
+            if (inputText[^1] == ' ')
+            {
+                enteredTokens.AddDefaultToken();
+            }
+            
+            string wordToComplete = enteredTokens.LastToken?.Value ?? "";
+            string baseText = inputText[..(inputText.Length - wordToComplete.Length)];
+
+            var results = Resolver.Suggestions(wordToComplete, enteredTokens, cursorPosition);
+            if (results.Count == 0 ) return default;
+
+            List<PredictiveSuggestion> predictiveSuggestions = new();
+            foreach (Suggestion result in results)
+            {
+                PredictiveSuggestion suggestion = new(baseText + result.CompletionText, result.ToolTip);
+                predictiveSuggestions.Add(suggestion);
+            }
+            SuggestionPackage suggestionPackage = new(predictiveSuggestions);
+
+            return suggestionPackage;
+        }
+
+        #region "interface methods for processing feedback"
+
+        /// <summary>
+        /// Gets a value indicating whether the predictor accepts a specific kind of feedback.
+        /// </summary>
+        /// <param name="client">Represents the client that initiates the call.</param>
+        /// <param name="feedback">A specific type of feedback.</param>
+        /// <returns>True or false, to indicate whether the specific feedback is accepted.</returns>
+        public bool CanAcceptFeedback(PredictionClient client, PredictorFeedbackKind feedback) => false;
+
+        /// <summary>
+        /// One or more suggestions provided by the predictor were displayed to the user.
+        /// </summary>
+        /// <param name="client">Represents the client that initiates the call.</param>
+        /// <param name="session">The mini-session where the displayed suggestions came from.</param>
+        /// <param name="countOrIndex">
+        /// When the value is greater than 0, it's the number of displayed suggestions from the list
+        /// returned in <paramref name="session"/>, starting from the index 0. When the value is
+        /// less than or equal to 0, it means a single suggestion from the list got displayed, and
+        /// the index is the absolute value.
+        /// </param>
+        public void OnSuggestionDisplayed(PredictionClient client, uint session, int countOrIndex) { }
+
+        /// <summary>
+        /// The suggestion provided by the predictor was accepted.
+        /// </summary>
+        /// <param name="client">Represents the client that initiates the call.</param>
+        /// <param name="session">Represents the mini-session where the accepted suggestion came from.</param>
+        /// <param name="acceptedSuggestion">The accepted suggestion text.</param>
+        public void OnSuggestionAccepted(PredictionClient client, uint session, string acceptedSuggestion) { }
+
+        /// <summary>
+        /// A command line was accepted to execute.
+        /// The predictor can start processing early as needed with the latest history.
+        /// </summary>
+        /// <param name="client">Represents the client that initiates the call.</param>
+        /// <param name="history">History command lines provided as references for prediction.</param>
+        public void OnCommandLineAccepted(PredictionClient client, IReadOnlyList<string> history) { }
+
+        /// <summary>
+        /// A command line was done execution.
+        /// </summary>
+        /// <param name="client">Represents the client that initiates the call.</param>
+        /// <param name="commandLine">The last accepted command line.</param>
+        /// <param name="success">Shows whether the execution was successful.</param>
+        public void OnCommandLineExecuted(PredictionClient client, string commandLine, bool success) { }
+
+        #endregion;
+    }
+
+    /// <summary>
+    /// Register the predictor on module loading and unregister it on module un-loading.
+    /// </summary>
+    public class Init : IModuleAssemblyInitializer, IModuleAssemblyCleanup
+    {
+        private const string Identifier = "55c982c1-00c3-4005-90fb-010d008fd3bd";
+
+        /// <summary>
+        /// Gets called when assembly is loaded.
+        /// </summary>
+        public void OnImport()
+        {
+            var predictor = new PoshPredictiveTextPredictor(Identifier);
+            SubsystemManager.RegisterSubsystem(SubsystemKind.CommandPredictor, predictor);
+        }
+
+        /// <summary>
+        /// Gets called when the binary module is unloaded.
+        /// </summary>
+        public void OnRemove(PSModuleInfo psModuleInfo)
+        {
+            SubsystemManager.UnregisterSubsystem(SubsystemKind.CommandPredictor, new Guid(Identifier));
+        }
+    }
+}
