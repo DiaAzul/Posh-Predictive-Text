aef238037ba84d07bfc77f30e5d26e30102294eb
David Plummer <28156158+DiaAzul@users.noreply.github.com>
2022-09-05 16:01:09 +0100

    BUG Add note to fix repeat suggestions for single positional parameters.

--- PoshPredictiveText/Resolver.cs
+++ PoshPredictiveText/Resolver.cs
@@ -127,6 +127,7 @@ namespace PoshPredictiveText
             
             // ----- POSITIONAL VALUES -----
             // If we have a helper for positional parameters then return the suggestions.
+            // BUG [ ][RESOLVER] If only one positional parameter allowed do not permit repeat suggstions.
             if (!listOnlyParameterValues && commandComplete)
             {
                 LOGGER.Write("Listing positional parameters.");
