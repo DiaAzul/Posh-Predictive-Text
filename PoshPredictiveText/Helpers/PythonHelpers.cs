b0bedad2b18dc37b4b201ecdd22c0f900ac1b3f2
David Plummer <28156158+DiaAzul@users.noreply.github.com>
2022-09-05 15:55:14 +0100

    TODO Add task to create a Python helper to parse the arguments in a py file.
    Python uses a standard library to parse CLI arguments. This knowledge may be used to parse a python file to extract completion information.

---
+++ PoshPredictiveText/Helpers/PythonHelpers.cs
@@ -0,0 +1,25 @@
+﻿// TODO [ ][PYTHON] Create helper to parse .py file for arguments.
+// Python has a standard library to define arguments, this knowledge can be
+// used to parse python files to identify arguments for predictive completion.
+// https://docs.python.org/3/library/argparse.html
+/*
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
+*/
+
+
+namespace PoshPredictiveText.Helpers
+{
+    internal class PythonHelpers
+    {
+    }
+}
