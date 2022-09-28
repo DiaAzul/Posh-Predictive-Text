0df0ab1d838297faa9ee88d84ca1b35c529d9c5d
David Plummer <28156158+DiaAzul@users.noreply.github.com>
2022-09-28 13:52:07 +0100

    Add state machine to tokeniser

---
+++ PoshPredictiveText/Tokeniser/StateMachine.cs
@@ -0,0 +1,55 @@
+﻿
+namespace PoshPredictiveText
+{
+    /// <summary>
+    /// The state machine evaluates tokens and adds
+    /// semantic information.
+    /// </summary>
+    internal class StateMachine
+    {
+        private readonly State state = State.NoCommand;
+        /// <summary>
+        /// Permissible states within the state machine
+        /// </summary>
+        enum State
+        {
+            NoCommand, // No command has been identified.
+            Item, // Process the token as command, option, parameter.
+            Value, // Process the token as a value
+            Inert, // No further processing required.
+        }
+
+        /// <summary>
+        /// Routing function passing token to evaluation method
+        /// dependent upon the state of the state machine.
+        /// </summary>
+        /// <param name="tokens">Input tokens for enhancement</param>
+        /// <returns>Enhanced tokens</returns>
+        internal List<Token> Evaluate(List<Token> tokens)
+        {
+            return state switch
+            {
+                State.NoCommand => NoCommand(tokens),
+                State.Item => EvaluateItem(tokens),
+                State.Value => EvaluateValue(tokens),
+                State.Inert => tokens,
+                _ => tokens,
+            };
+        }
+
+        internal static List<Token> NoCommand(List<Token> tokens)
+        {
+            return tokens;
+        }
+
+        internal static List<Token> EvaluateItem(List<Token> tokens)
+        {
+            return tokens;
+        }
+
+        internal static List<Token> EvaluateValue(List<Token> tokens)
+        {
+            return tokens;
+        }
+    }
+}
